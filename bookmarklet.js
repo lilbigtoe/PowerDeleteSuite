d404ebc7e5360bc1a6e61ee02055b547158d00e8
robert gama <lilbigtoe@roberts-MacBook.local>
2026-04-26 10:40:48 -0700

    Update all URLs to point to master branch

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,7 +4,7 @@ javascript: (function () {
     document.location.hostname.split(".").slice(-2).join(".") === "reddit.com";
   var isOverview = document.location.href.match(/\/overview\b/);
   if (isReddit && isOverview) {
-    var cachBustUrl = 'https://raw.githubusercontent.com/lilbigtoe/PowerDeleteSuite/refactor/module-split/powerdeletesuite.js?' + (new Date().getDate());
+    var cachBustUrl = 'https://raw.githubusercontent.com/lilbigtoe/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
     fetch(cachBustUrl)
       .then(function (response) {
         return response.text();
